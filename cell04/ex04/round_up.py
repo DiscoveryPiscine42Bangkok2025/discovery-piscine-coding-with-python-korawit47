5ab7193a4b40f46442d5e1ba7dab4974172e1b16
KRS_GOLF <korawit7680@gmail.com>
2025-09-17 19:21:24 +0700

    .

--- cell04/ex04/round_up.py
+++ cell04/ex04/round_up.py
@@ -1,2 +1,7 @@
-num = float(input("Give me a number: "))
-print(round(num))
+user_input = input("Give me a number: ")
+
+try :
+    print(int(user_input))
+except ValueError :
+    user_float = float(user_input)
+    print(user_float.__ceil__())
