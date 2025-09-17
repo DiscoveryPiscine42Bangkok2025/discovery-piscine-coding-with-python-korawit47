19a7527d567d64a3dbe4c79fdc4fbd6a11342d64
KRS_GOLF <korawit7680@gmail.com>
2025-09-18 00:19:54 +0700

    .

--- cell05/ex07/downcase_it.py
+++ cell05/ex07/downcase_it.py
@@ -1,8 +1,6 @@
 import sys
-if len(sys.argv) == 1:
-    print("none")
-elif len(sys.argv) == 2:
-    letter = sys.argv[1].lower()
-    print(letter)
+
+if len(sys.argv) == 2:
+    print(sys.argv[1].lower())
 else:
     print("none")
